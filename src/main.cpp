e3ebcd6954a6dd874bb247ff1e53d1385fe373cb
Jozef Mlich <jmlich83@gmail.com>
2022-09-23 08:57:30 +0200

    find . -iname '*.h' -o -iname '*.cpp' -exec clang-format -i {} \;

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,22 @@
 #include <QtQuick>
 #endif
 
-#include <QtGui/QGuiApplication>
 #include <QApplication>
+#include <QtGui/QGuiApplication>
 #include <QtQml>
 #include <QtQuick/QQuickView>
 
 #include <glacierapp.h>
 
-
-Q_DECL_EXPORT int main(int argc, char *argv[])
+Q_DECL_EXPORT int main(int argc, char* argv[])
 {
-    QApplication *app = new QApplication(argc, argv);
+    QApplication* app = new QApplication(argc, argv);
 
     QFileInfo exe = QFileInfo(app->applicationFilePath());
     app->setApplicationName(exe.fileName());
 
     QTranslator* myappTranslator = new QTranslator(app);
-    if (myappTranslator->load(QLocale(), app->applicationName(), QLatin1String("_"), QLatin1String("/usr/share/%1/translations/").arg(app->applicationName()) )) {
+    if (myappTranslator->load(QLocale(), app->applicationName(), QLatin1String("_"), QLatin1String("/usr/share/%1/translations/").arg(app->applicationName()))) {
         qDebug() << "translation.load() success" << QLocale::system().name();
         if (app->installTranslator(myappTranslator)) {
             qDebug() << "installTranslator() success" << QLocale::system().name();
@@ -29,10 +28,9 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
         qDebug() << "translation.load() failed" << QLocale::system().name();
     }
 
-
-// QGuiApplication *app = GlacierApp::app(argc, argv);
+    // QGuiApplication *app = GlacierApp::app(argc, argv);
     app->setOrganizationName("NemoMobile");
-    QQuickWindow *window = GlacierApp::showWindow();
+    QQuickWindow* window = GlacierApp::showWindow();
     window->setTitle(QObject::tr("Hardware test"));
     window->setIcon(QIcon("/usr/share/glacier-testtool/icons/glacier-testtool.png"));
 
